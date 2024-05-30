f398afd48ad29499645a41131163d16d16f27541
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-05-30 22:09:56 +0530

    Complete backend using jwt mongodb and authentication, done in week 3 03-mongo and 04-mongo-with-jwt-authentication

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,27 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { string } = require("zod");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(
+  "mongodb+srv://rikteshs20:Rik%403708@cluster0.qi4wq4o.mongodb.net/course_selling_app"
+);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
+  purchasedCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  title: String,
+  Description: String,
+  imageLink: String,
+  price: Number,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
